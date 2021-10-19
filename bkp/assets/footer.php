94562aa8aa459311519432cc0d54c833c4697ebe
carohansen73 <laaflaka.8@hotmail.com>
2021-10-19 14:55:08 -0300

    add pagina old

---
+++ bkp/assets/footer.php
@@ -0,0 +1,87 @@
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="foot-sect col-xs-12 col-md-6 col-sm-6 col-lg-3">
+          <div class="foot-title">NEWSLETTER</div>
+          <form action="#" id="email-footer-form" role="form" method="post" novalidate="novalidate">
+            <div class="form-group">
+              <input type="email" id="fvi" class="form-control" placeholder="Tu Email:" required>
+              <!-- <label for="fvi" class="control-label"></label> -->
+            </div>
+            <div id="loader-subscribe-form" class="form-loader hidden">
+              <div class="loader">
+              </div>
+            </div>
+            <button type="submit" class="btn btn-darkened pull-left">Suscribirse</button>
+          </form>
+          <div id="success-subscribe" class="success-message hidden">
+            <div class="suc-title"><i class="fa fa-check"></i>Thank you!</div>
+          </div>
+        </div>
+        <div class="foot-sect col-xs-12 col-md-6 col-sm-6 col-lg-3">
+          <div class="foot-title">SERVICIOS</div>
+          <ul class="footer__ulist">
+            <li><a href="#">Gu&iacute;a de tramites</a> </li>
+            <li><a href="#">Formularios</a> </li>
+            <li><a href="#">Marco Jur&iacute;dico</a> </li>
+            <li><a href="#">Atenci&oacute;n al vecino</a> </li>
+            <li><a href="#">Lugares y medios de pago</a> </li>
+          </ul>
+        </div>
+        <div class="clearfix visible-sm-block visible-md-block"></div>
+        <div class="foot-sect col-xs-12 col-md-6 col-sm-6 col-lg-3">
+          <div class="foot-title">CONTACTO</div>
+          <ul class="footer__icon-list">
+            <li class="icon-list__item icon-list__item-baloone">Av Rivadavia 1, C.P. 7500
+              <br>Tres Arroyos, Buenos Aires</li>
+            <li class="icon-list__item icon-list__item-phone">(02983) 439200</li>
+            <li class="icon-list__item icon-list__item-letter"><a href="mailto:reclamos@tresarroyos.gov.ar">@tresarroyos.gov.ar</a> </li>
+          </ul>
+        </div>
+        <div class="foot-sect col-xs-12 col-md-6 col-sm-6 col-lg-3">
+          <div class="foot-title">INSTITUCIONAL</div>
+          <p>Yet as Philip Ball points out, the history of camouflage and our limited understanding of how creatures hide in nature may make the task trickier than it seems.</p>
+          <div class="copyrights copyrights__footer">Copyright © 2015. All Rights reserved.</div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <script src="js/jquery/jquery-1.11.1.min.js"></script>
+  <script src="js/otherlibs/bootstrap/bootstrap.js"></script>
+  <script src="js/jquery/plugins/Chart.js"></script>
+  <script src="js/jquery/plugins/bootstrap-select.js"></script>
+  <script src="js/jquery/plugins/slick/slick.js"></script>
+  <script src="js/jquery/plugins/topEventsAnimate.js"></script>
+  <script src="js/jquery/plugins/skrollr.js"></script>
+  <script src="js/jquery/plugins/jquery.sharrre.min.js"></script>
+  <script src="js/jquery/plugins/jquery.parallax.min.js"></script>
+  <script src="js/jquery/plugins/jquery.form.js"></script>
+  <script src="js/jquery/plugins/validation/jquery.validate.js"></script>
+  <script src="js/app/lib/site.js"></script>
+  <script src="js/app/lib/siteMode.js"></script>
+  <script src="js/app/mode/themeMode.js"></script>
+  <script src="js/app/modules/mainSlider.js"></script>
+  <script src="js/app/modules/menuAligns.js"></script>
+  <script src="js/app/modules/bgStretch.js"></script>
+  <script src="js/app/modules/eventAnimate.js"></script>
+  <script src="js/app/modules/stuffSlider.js"></script>
+  <script src="js/app/modules/gridsDiagramms.js"></script>
+  <script src="js/app/modules/barChart.js"></script>
+  <script src="js/app/modules/disquis.js"></script>
+  <script src="js/app/modules/stickyHeader.js"></script>
+  <script src="js/app/modules/globalScripts.js"></script>
+  <script src="js/app/modules/parallax.js"></script>
+  <script src="js/app/modules/backgroundImg.js"></script>
+  <script src="js/app/modules/scrollPlugin.js"></script>
+  <script src="js/app/modules/googleMap.js"></script>
+  <script src="js/app/modules/eventSlider.js"></script>
+  <script src="js/app/modules/stuffPage.js"></script>
+  <script src="js/app/modules/aboutDiagramms.js"></script>
+  <script src="js/app/modules/sharrre.js"></script>
+  <script src="js/app/modules/dtMenu.js"></script>
+  <script src="js/app/modules/formSubscribe.js"></script>
+  <script src="js/app/modules/formFieldsSubscribe.js"></script>
+  <script src="js/init.js"></script>
+</body>
+
+</html>
---
+++ bkp/assets/header.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Dir. de Politicas Tributarias</title>
+  <meta name="description" content="" />
+  <meta name="keywords" content="" />
+  <base href="" />
+  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta name="mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-status-bar-style" content="black">
+  <meta name="apple-mobile-web-app-title" content="">
+  <meta property="og:title" content="" />
+  <meta property="og:description" content="" />
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content="" />
+  <meta property="og:image" content="" />
+  <meta property="og:site_name" content="" />
+  <link rel="dns-prefetch" href="http://code.jquery.com" />
+  <link rel="dns-prefetch" href="http://fonts.googleapis.com" />
+  <link rel="dns-prefetch" href="http://www.google-analytics.com" />
+  <link rel="icon" href="images/themegovrnmnt-favicon.ico" type="image/x-icon" />
+  <link rel="shortcut icon" href="images/themegovrnmnt-favicon.ico" type="image/x-icon" />
+  <link rel="stylesheet" href="css/min/style.min.css">
+  <link rel="stylesheet" href="js/jquery/plugins/slick/slick.css">
+  <!--<link rel="stylesheet" href="css/color/sandybay-color.min.css">
+  <link rel="stylesheet" href="css/color/skyline-color.min.css">-->
+  <link rel="stylesheet" href="css/color/raspberry-color.min.css">
+  <link rel="stylesheet" href="css/estilo.css">
+  <!--[if lt IE 9]>
+            <script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+        <![endif]-->
+  <!--[if IE]>
+            <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+</head>
+<body id="body">
+  <header>
+      <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-2 col-lg-offset-1 navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <div class="home-btn">Home</div>
+          <a href="index.php" class="navbar-brand">
+            <img src="images/logo.png" alt="" class="img-responsive" style="max-width: 230px;">
+          </a>
+          <div class="line"></div>
+        </div>
+        <div class="col-lg-7 col-lg-offset-1 navbar-collapse collapse">
+          <nav role="navigation">
+            <ul id="navigation" class="nav navbar-nav navbar-right dl-menu">
+              <li>
+                <a href="#">Institucional</a>
+                <div class="clearfix"></div>
+              </li>
+              <li>
+                <a href="#">Nosotros</a>
+                <div class="clearfix"></div>
+              </li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" role="button" aria-expanded="false">Servicios <i>›</i></a>
+                <ul class="dropdown-menu dl-submenu" role="menu">
+                  <li><a href="news.html">GU&Iacute;a de Tr&aacute;mites</a>
+                  </li>
+                  <li><a href="news-no-image.html">Formulario</a></li>
+                  <li><a href="news-no-image.html">Marco  jur&iacute;dico</a></li>
+                  <li><a href="news-no-image.html">Atenci&oacute;n al vecino</a></li>
+                  <li><a href="news-no-image.html">Lugares y medios de pago</a></li>
+                        <!--	
+						<li class="dropdown-submenu">
+                    <a href="news-view.html" class="dropdown-toggle" role="button" aria-expanded="false">News view <i>›</i></a>
+                    <ul class="dropdown-menu dl-submenu" role="menu">
+                      <li><a href="#">News view 3 level</a>
+                      </li>
+                    </ul>
+                  </li>
+          										-->
+                </ul>
+                <div class="clearfix"></div>
+              </li>
+              <li>
+                <a href="stuff.html">Deudores</a>
+                <div class="clearfix"></div>
+              </li>
+              <li>
+                <a href="#" >Vencimientos</a>
+                <div class="clearfix"></div>
+              </li>
+              <li>
+                <a href="contacts.html">Contacto</a>
+                <div class="clearfix"></div>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </header>
